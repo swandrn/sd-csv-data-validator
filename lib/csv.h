c35aab7dd3e84c17adabbafcfe7ead4f92bb46d6
swandrn <159421702+swandrn@users.noreply.github.com>
2026-03-22 19:20:33 +0100

    chore: print path in validate function

--- lib/csv.h
+++ lib/csv.h
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 static int validate(const char *path) {
-  printf("hello from validate\n");
+  printf("hello from validate with path=%s\n", path);
   return 0;
 }
 #endif // CSV_IMPLEMENTATION
