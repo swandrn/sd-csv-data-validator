4a54db76e35f4004443bb13fc5474386ca8bc659
swandrn <159421702+swandrn@users.noreply.github.com>
2026-03-22 21:38:05 +0100

    feat: verify that the file ends with .csv

--- lib/csv.h
+++ lib/csv.h
@@ -5,6 +5,26 @@
 #define CSV_IMPLEMENTATION // Enables syntax highlighting - Developement only
 #ifdef CSV_IMPLEMENTATION
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Checks that a file is a CSV, exits with non zero otherwise
+static void file_is_csv(const char *path) {
+  // ends with .csv
+  const char *ext = ".csv";
+  size_t path_len = strlen(path);
+  size_t ext_len = strlen(ext);
+  if (ext_len > path_len) {
+    fprintf(stderr,
+            "length of file extension (%zu) is longer than file path (%zu)\n",
+            ext_len, path_len);
+    exit(EXIT_FAILURE);
+  }
+  if (strncmp(path + path_len - ext_len, ext, ext_len) != 0) {
+    fprintf(stderr, "file extension is not .csv\n");
+    exit(EXIT_FAILURE);
+  }
+}
 
 static int validate(const char *path) {
   printf("hello from validate with path=%s\n", path);
