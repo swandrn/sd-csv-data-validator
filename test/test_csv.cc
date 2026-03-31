91c0d0437476e72cba1f4ddc643c6f3fce4238d9
swandrn <159421702+swandrn@users.noreply.github.com>
2026-03-31 20:05:03 +0200

    test: read csv with delimiter inside quotes

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -216,3 +216,13 @@ TEST_F(ReadCsvValid, DatetimeLikeValues) {
   EXPECT_EQ(csv->rows[1].fields[0].field_type, STRING_TYPE);
   EXPECT_EQ(csv->rows[2].fields[0].field_type, STRING_TYPE);
 }
+
+TEST_F(ReadCsvValid, DelimiterInsideQuotes) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_delimiter_inside_quotes.csv"),
+            3);
+  EXPECT_EQ(csv->rows[0].fields[0].field_type, STRING_TYPE);
+  EXPECT_EQ(csv->rows[1].fields[0].field_type, INT_TYPE);
+  EXPECT_EQ(csv->rows[1].fields[1].field_type, STRING_TYPE);
+  EXPECT_EQ(csv->rows[2].fields[0].field_type, INT_TYPE);
+  EXPECT_EQ(csv->rows[2].fields[1].field_type, STRING_TYPE);
+}
