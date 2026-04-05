1432cdb869ad5eef85e93a7b6fff65a59e34f3f6
swandrn <159421702+swandrn@users.noreply.github.com>
2026-04-05 22:15:05 +0200

    test: read csv with integers only

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -283,3 +283,13 @@ TEST_F(ReadCsvValid, FloatsOnly) {
   EXPECT_EQ(csv->rows[2].size, 2);
   EXPECT_EQ(csv->rows[2].fields[0].field_type, FLOAT_FIELD);
 }
+
+TEST_F(ReadCsvValid, IntegersOnly) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_integers_only.csv"), 3);
+  EXPECT_EQ(csv->rows[0].size, 3);
+  EXPECT_EQ(csv->rows[0].fields[0].field_type, STRING_FIELD);
+  EXPECT_EQ(csv->rows[1].size, 3);
+  EXPECT_EQ(csv->rows[1].fields[0].field_type, INT_FIELD);
+  EXPECT_EQ(csv->rows[2].size, 3);
+  EXPECT_EQ(csv->rows[2].fields[0].field_type, INT_FIELD);
+}
