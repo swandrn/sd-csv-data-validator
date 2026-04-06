8feff17b157ea2eca0457d226b93c9857587c31c
swandrn <159421702+swandrn@users.noreply.github.com>
2026-04-06 20:27:01 +0200

    test: read csv with unicode text

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -342,3 +342,11 @@ TEST_F(ReadCsvValid, SymbolsAndPunctuation) {
   EXPECT_EQ(csv->rows[1].size, 1);
   EXPECT_EQ(csv->rows[2].size, 1);
 }
+
+TEST_F(ReadCsvValid, UnicodeText) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_unicode_text.csv"), 4);
+  EXPECT_EQ(csv->rows[0].size, 2);
+  EXPECT_EQ(csv->rows[1].size, 2);
+  EXPECT_EQ(csv->rows[2].size, 2);
+  EXPECT_EQ(csv->rows[3].size, 2);
+}
