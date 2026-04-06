e5e723fc4363d6b23c73409052196c16536f535c
swandrn <159421702+swandrn@users.noreply.github.com>
2026-04-06 12:52:41 +0200

    test: read csv with many columns

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -318,3 +318,10 @@ TEST_F(ReadCsvValid, LongTextFields) {
   EXPECT_EQ(csv->rows[1].size, 2);
   EXPECT_EQ(csv->rows[2].size, 2);
 }
+
+TEST_F(ReadCsvValid, ManyColumns) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_many_columns.csv"), 3);
+  EXPECT_EQ(csv->rows[0].size, 32);
+  EXPECT_EQ(csv->rows[1].size, 32);
+  EXPECT_EQ(csv->rows[2].size, 32);
+}
