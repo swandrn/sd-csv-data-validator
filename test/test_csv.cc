e29d06ec65a90bdde48dddd82f68084405586943
swandrn <159421702+swandrn@users.noreply.github.com>
2026-04-06 19:36:18 +0200

    feat: read csv with null like literals

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -234,3 +234,12 @@ TEST_F(ReadCsvValid, NegativeNumbers) {
   EXPECT_EQ(csv->rows[1].size, 2);
   EXPECT_EQ(csv->rows[2].size, 2);
 }
+
+TEST_F(ReadCsvValid, NullLikeLiterals) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_null_like_literals.csv"), 5);
+  EXPECT_EQ(csv->rows[0].size, 1);
+  EXPECT_EQ(csv->rows[1].size, 1);
+  EXPECT_EQ(csv->rows[2].size, 1);
+  EXPECT_EQ(csv->rows[3].size, 1);
+  EXPECT_EQ(csv->rows[4].size, 1);
+}
