c730016d34ff6d3307eace5d0f4586acbcd5842c
swandrn <159421702+swandrn@users.noreply.github.com>
2026-04-06 20:23:32 +0200

    test: read csv with special characters

--- test/test_csv.cc
+++ test/test_csv.cc
@@ -327,3 +327,10 @@ TEST_F(ReadCsvValid, SpaceOnlyField) {
   EXPECT_EQ(csv->rows[1].size, 1);
   EXPECT_EQ(csv->rows[2].size, 1);
 }
+
+TEST_F(ReadCsvValid, SpecialCharacters) {
+  EXPECT_EQ(read_csv(&r, csv, "./csv_valid/valid_special_characters.csv"), 3);
+  EXPECT_EQ(csv->rows[0].size, 1);
+  EXPECT_EQ(csv->rows[1].size, 1);
+  EXPECT_EQ(csv->rows[2].size, 1);
+}
